f96e16d765e04671888dfd6d9bc045d354acae21
Colton - Work <colthyland@gmail.com>
2024-02-20 13:09:48 -0800

    init anagrams

--- compress/main.js
+++ compress/main.js
@@ -11,9 +11,27 @@
 // You can assume that the input only contains alphabetic characters.
 
 const compress = (s) => {
-    // todo
+    let i = 0;
+    let j = 0;
+    let result = [];
+    while (j <= s.length) {
+        if (s[i] == s[j]) {
+            j++;
+        } else {
+            let num = s.slice(i, j).length;
+            if (num !== 1) result.push(num);
+            result.push(s[i]);
+            i = j;
+        }
+    }
+
+    return result.join('');
 };
 
 module.exports = {
     compress,
 };
+
+// n = length of string
+// Time: O(n)
+// Space: O(n)
