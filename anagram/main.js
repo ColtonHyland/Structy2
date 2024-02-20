057e39a7566d27270d32d98ebd94c7e5cbee0470
Colton - Work <colthyland@gmail.com>
2024-02-20 13:29:20 -0800

    soln anagrams

--- anagram/main.js
+++ anagram/main.js
@@ -4,12 +4,40 @@
 // Anagrams are strings that contain the same characters, but in any order.
 
 const anagrams = (s1, s2) => {
-    // todo
+    const count = {};
+    for (let char of s1) {
+        if (!(char in count)) {
+            count[char] = 0;
+        }
+        count[char] += 1;
+    }
+
+    for (let char of s2) {
+        if (char in count) {
+            count[char] -= 1;
+        } else {
+            return false;
+        }
+    }
+
+    for (let char in count) {
+        if (count[char] !== 0) {
+            return false;
+        }
+    }
+
+    return true;
 };
 
 module.exports = {
     anagrams,
 };
+
+
+// let n = length of s1
+// let m = length of s2
+// Time: O(n + m)
+// Space: ~O(n + m)
 
 // anagrams('restful', 'fluster'); // -> true
 // anagrams('cats', 'tocs'); // -> false
