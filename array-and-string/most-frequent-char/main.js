3fee5055edf0261c6646b6a2c8bdb6d4f2c8cf84
Colton - Work <colthyland@gmail.com>
2024-02-21 10:28:55 -0800

    soln most freq

--- array-and-string/most-frequent-char/main.js
+++ array-and-string/most-frequent-char/main.js
@@ -6,12 +6,31 @@
 // You can assume that the input string is non-empty.
 
 const mostFrequentChar = (s) => {
-    // todo
+    const count = {};
+    for (let char of s) {
+        if (!(char in count)) {
+            count[char] = 0;
+        }
+        count[char] += 1;
+    }
+
+    let best = null;
+    for (let char of s) {
+        if (best === null || count[char] > count[best]) {
+            best = char;
+        }
+    }
+
+    return best;
 };
 
 module.exports = {
     mostFrequentChar,
 };
+
+// let n = length of string
+// Time: O(n)
+// Space: O(n)
 
 
 // mostFrequentChar('bookeeper'); // -> 'e'
