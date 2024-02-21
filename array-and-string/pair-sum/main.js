12f8b8bd70e1871adf5041ff1dbe5ba3d7bdf72b
Colton - Work <colthyland@gmail.com>
2024-02-21 11:33:59 -0800

    soln pair sum

--- array-and-string/pair-sum/main.js
+++ array-and-string/pair-sum/main.js
@@ -8,12 +8,24 @@
 // There is guaranteed to be one such pair that sums to the target.
 
 const pairSum = (numbers, targetSum) => {
-    // todo
+    const prev = {};
+    for (let i = 0; i < numbers.length; i++) {
+        const num = numbers[i];
+        const diff = targetSum - num;
+        if (diff in prev) {
+            return [i, prev[diff]];
+        }
+        prev[num] = i;
+    }
 };
 
 module.exports = {
     pairSum,
 };
+
+// n = numbers length
+// Time: O(n)
+// Space: O(n)
 
 // pairSum([3, 2, 5, 4, 1], 8); // -> [0, 2]
 // pairSum([4, 7, 9, 2, 5, 1], 5); // -> [0, 5]
