f7c7fca824c99c241f40fbefebf62fb960363387
Colton - Work <colthyland@gmail.com>
2024-02-22 10:32:39 -0800

    soln linked list find

--- linked-list/linked-list-find/main.js
+++ linked-list/linked-list-find/main.js
@@ -10,9 +10,29 @@
 //   }
 // }
 
+// const linkedListFind = (head, target) => {
+//     let current = head;
+//     while (current !== null) {
+//         if (current.val === target) return true;
+//         current = current.next;
+//     }
+//     return false;
+// };
+
+// n = number of nodes
+// TIme: O(n)
+// Space: O(1)
+
+//RECURSIVE
 const linkedListFind = (head, target) => {
-  // todo
+    if (head === null) return false;
+    if (head.val === target) return true;
+    return linkedListFind(head.next, target);
 };
+
+// n = number of nodes
+// TIme: O(n)
+// Space: O(n)
 
 module.exports = {
   linkedListFind,
