db08e4701e2e72aef1edddc07ef93de637e4d1ea
Colton - Work <colthyland@gmail.com>
2024-02-22 10:51:34 -0800

    soln get node value

--- linked-list/get-node-value/main.js
+++ linked-list/get-node-value/main.js
@@ -11,9 +11,33 @@
 //   }
 // }
 
+// const getNodeValue = (head, index) => {
+//     let current = head;
+//     while (current !== null) {
+//         if (index === 0) return current.val;
+//         current = current.next;
+//         index--;
+//     }
+
+//     return null;
+// }
+
+// n = number of nodes
+// TIme: O(n)
+// Space: O(1)
+
+//RECURSIVE
 const getNodeValue = (head, index) => {
-  // todo
+    if (head === null) return null;
+    if (index === 0) return head.val;
+    return getNodeValue(head.next, index - 1);
 };
+
+// n = number of nodes
+// TIme: O(n)
+// Space: O(n)
+
+
 
 module.exports = {
   getNodeValue,
