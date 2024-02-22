2614ab480727257ef3c6b73180052def77d4f13a
Colton - Work <colthyland@gmail.com>
2024-02-22 10:23:39 -0800

    soln sum list

--- linked-list/sum-list/main.js
+++ linked-list/sum-list/main.js
@@ -9,9 +9,29 @@
 //   }
 // }
 
+// const sumList = (head) => {
+//     let sum = 0;
+//     let current = head;
+//     while (current !== null) {
+//         sum += current.val;
+//         current = current.next;
+//     }
+
+//     return sum;
+// };
+
+// n = number of nodes
+// TIme: O(n)
+// Space: O(1)
+
 const sumList = (head) => {
-  // todo
+    if (head === null) return 0;
+    return head.val + sumList(head.next);
 };
+
+// n = number of nodes
+// TIme: O(n)
+// Space: O(n)
 
 module.exports = {
   sumList,
