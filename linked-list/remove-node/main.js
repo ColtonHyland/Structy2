b0858847eaaf0ebe1b862b4332846141859d68ed
Colton - Work <colthyland@gmail.com>
2024-02-27 15:22:50 -0800

    soln remove node

--- linked-list/remove-node/main.js
+++ linked-list/remove-node/main.js
@@ -19,8 +19,24 @@ class Node {
 }
 
 const removeNode = (head, targetVal) => {
-  // todo
+    if (head.val === targetVal) return head.next;
+    let current = head;
+    let prev = null;
+    while (current !== null) {
+        if (current.val === targetVal) {
+            prev.next = current.next;
+            break;
+        }
+        prev = current;
+        current = current.next;
+    }
+
+    return head;
 };
+
+// n = list length
+// Time: O(n)
+// Space: O(1)
 
 module.exports = {
   removeNode,
