64123685ad18109a01eb14593349cda9a39bddd9
Colton - Work <colthyland@gmail.com>
2024-02-26 16:23:12 -0800

    init merge lists

--- linked-list/zipper-lists/main.js
+++ linked-list/zipper-lists/main.js
@@ -44,6 +44,22 @@ const zipperLists = (head1, head2) => {
 //Time: O(min(m,n))
 //Space: O(1)
 
+// const zipperLists = (head1, head2) => {
+//     if (head1 === null && head2 === null) return null;
+//     if (head1 === null) return head2;
+//     if (head2 === null) return head1;
+//     const next1 = head1.next;
+//     const next2 = head2.next;
+//     head1.next = head2;
+//     head2.next = zipperLists(next1, next2);
+//     return head1;
+// };
+
+// n = length of head1
+// m = length of head2
+//Time: O(min(m,n))
+//Space: O(min(m,n))
+
 module.exports = {
     zipperLists,
 };
