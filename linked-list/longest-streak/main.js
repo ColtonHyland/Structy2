33da2cb07496a76c75c4e256894df7123942051f
Colton - Work <colthyland@gmail.com>
2024-02-27 11:39:05 -0800

    soln longest streak

--- linked-list/longest-streak/main.js
+++ linked-list/longest-streak/main.js
@@ -11,8 +11,26 @@ class Node {
 }
 
 const longestStreak = (head) => {
-  // todo
+    let current = head;
+    let prevVal = null;
+    let longest = 0;
+    let streak = 0;
+    while (current !== null) {
+        if (current.val === prevVal) {
+            streak += 1;
+        } else {
+            streak = 1;
+        }
+        if (streak > longest) longest = streak;
+        prevVal = current.val;
+        current = current.next;
+    }
+    return longest;
 };
+
+// n = length of list
+// Time: O(n)
+// Space: O(1)
 
 module.exports = {
   longestStreak,
