4857e61d3f2fa4febcf44bd57a5ad9990eb1c17e
Colton - Work <colthyland@gmail.com>
2024-02-29 12:40:08 -0800

    xoln create linked list

--- linked-list/create-linked-list/main.js
+++ linked-list/create-linked-list/main.js
@@ -11,8 +11,30 @@ class Node {
 }
 
 const createLinkedList = (values) => {
-  // todo
+    if (values.length === 0) return null;
+    const dummyHead = new Node(null);
+    let tail = dummyHead;
+    for (let value of values) {
+        tail.next = new Node(value);
+        tail = tail.next;
+    }
+    return dummyHead.next;
 };
+
+// n is lnegth of values
+// Time: O(n)
+// Space: O(n)
+
+// const createLinkedList = (values, count = 0) => {
+//     if (values.length === count) return null;
+//     const newNode = new Node(values[count]);
+//     newNode.next = createLinkedList(values, count + 1);
+//     return newNode;
+// };
+
+// n is lnegth of values
+// Time: O(n)
+// Space: O(n)
 
 module.exports = {
   createLinkedList,
