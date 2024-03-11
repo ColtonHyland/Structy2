501f2b215ec21b28031b394684f3d343baeddd2d
Colton - Work <colthyland@gmail.com>
2024-03-11 09:51:22 -0700

    soln depth first vals

--- binary-tree/depth-first-values/main.js
+++ binary-tree/depth-first-values/main.js
@@ -13,8 +13,33 @@
 // }
 
 const depthFirstValues = (root) => {
-    // todo
+  const stack = [ root ];
+  const vals = [];
+  if (root === null) return vals;
+  while (stack.length > 0) {
+    const current = stack.pop();
+    vals.push(current.val);
+    if (current.right !== null) stack.push(current.right);
+    if (current.left !== null) stack.push(current.left);
+
+  }
+  return vals;
 };
+
+// n = number of nodes
+// Time: O(n)
+// Space: O(n)
+
+// const depthFirstValues = (root) => {
+//     if (root === null) return [];
+//     const leftVals = depthFirstValues(root.left);
+//     const rightVals = depthFirstValues(root.right);
+//     return [root.val, ...leftVals, ...rightVals];
+// };
+
+// n = number of nodes
+// Time: O(n^2)
+// Space: O(n)
 
 module.exports = {
     depthFirstValues,
