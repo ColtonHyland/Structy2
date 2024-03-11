cb2bb31ee4e7b3eb31e5f0039d9a30f6edea5fb5
Colton - Work <colthyland@gmail.com>
2024-03-11 11:52:25 -0700

    soln max root to leaf

--- binary-tree/max-root-to-leaf-path-sum/main.js
+++ binary-tree/max-root-to-leaf-path-sum/main.js
@@ -12,8 +12,15 @@
 // }
 
 const maxPathSum = (root) => {
-  // todo
+    if (root === null) return -Infinity;
+    if (root.left === null && root.right === null) return root.val;
+    const childrenPathSum = Math.max(maxPathSum(root.left), maxPathSum(root.right));
+    return root.val + childrenPathSum;
 };
+
+// n = number of nodes
+// Time: O(n)
+// Space: O(n)
 
 module.exports = {
   maxPathSum,
