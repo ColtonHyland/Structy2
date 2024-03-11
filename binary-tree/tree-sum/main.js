612a30bb61933f098ea364204812a6c638ccae85
Colton - Work <colthyland@gmail.com>
2024-03-11 10:45:59 -0700

    soln tree sum

--- binary-tree/tree-sum/main.js
+++ binary-tree/tree-sum/main.js
@@ -12,8 +12,30 @@
 // }
 
 const treeSum = (root) => {
-  // todo
+    if (root === null) return 0;
+    let sum = 0;
+    const stack = [root];
+    while (stack.length > 0) {
+        const current = stack.pop();
+        sum += current.val;
+        if (current.right !== null) stack.push(current.right);
+        if (current.left !== null) stack.push(current.left);
+    }
+    return sum;
 };
+
+// n = number of nodes
+// Time: O(n)
+// Space: O(n)
+
+// const treeSum = (root) => {
+//     if (root === null) return 0;
+//     return root.val + treeSum(root.left) + treeSum(root.right);
+// };
+
+// n = number of nodes
+// Time: O(n)
+// Space: O(n)
 
 module.exports = {
   treeSum,
