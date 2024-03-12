c0c950becd8ba84c5499f07d51a955101714a6c7
Colton - Work <colthyland@gmail.com>
2024-03-12 12:03:45 -0700

    soln bottom right value

--- binary-tree/bottom-right-value/main.js
+++ binary-tree/bottom-right-value/main.js
@@ -14,8 +14,19 @@
 // }
 
 const bottomRightValue = (root) => {
-  // todo
+  const queue = [root];
+  let current = null;
+  while (queue.length > 0) {
+    current = queue.shift();
+    if (current.left !== null) queue.push(current.left);
+    if (current.right !== null) queue.push(current.right);
+  }
+  return current.val;
 };
+
+// n = number of nodes
+// Time: O(n^2)
+// Space: O(n)
 
 module.exports = {
   bottomRightValue,
