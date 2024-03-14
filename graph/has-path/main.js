c9bfef1b846220603812384fbae5d6372be3c64d
Colton - Work <colthyland@gmail.com>
2024-03-14 14:35:04 -0700

    soln has path

--- graph/has-path/main.js
+++ graph/has-path/main.js
@@ -7,8 +7,36 @@
 // Hey. This is our first graph problem, so you should be liberal with watching the Approach and Walkthrough. Be productive, not stubborn. -AZ
 
 const hasPath = (graph, src, dst) => {
-    // todo
+    if (src === dst) return true;
+    for (let neighbor of graph[src]) {
+        if (hasPath(graph, neighbor, dst) === true) {
+            return true;
+        }
+    }
+    return false;
 };
+
+// n = number of nodes
+// e = number edges
+// Time: O(e)
+// Space: O(n)
+
+// const hasPath = (graph, src, dst) => {
+//     const queue = [src];
+//     while (queue.length > 0) {
+//         const current = queue.shift();
+//         if (current === dst) return true;
+//         for (let neighbor of graph[current]) {
+//             queue.push(neighbor);
+//         }
+//     }
+//     return false;
+// };
+
+// n = number of nodes
+// e = number edges
+// Time: O(e)
+// Space: O(n^2)
 
 module.exports = {
     hasPath,
