bcb0558acb07cd6bb2404c3f3f26a9bcd660bba6
Colton - Work <colthyland@gmail.com>
2024-03-15 11:20:06 -0700

    soln largest component

--- graph/largest component/main.js
+++ graph/largest component/main.js
@@ -4,8 +4,29 @@
 // component in the graph.
 
 const largestComponent = (graph) => {
-    // todo
+    const visited = new Set();
+    let largestSize = 0;
+    for (let node in graph) {
+        let size = traverseGraph(graph, node, visited);
+        if (size > largestSize) largestSize = size;
+    }
+    return largestSize;
 };
+
+const traverseGraph = (graph, current, visited) => {
+    if (visited.has(String(current))) return 0;
+    visited.add(String(current));
+    let size = 1;
+    for (let neighbor of graph[current]) {
+        size += traverseGraph(graph, neighbor, visited, size + 1);
+    }
+    return size;
+};
+
+// n = number of nodes
+// e = number edges
+// Time: O(e)
+// Space: O(n)
 
 module.exports = {
     largestComponent,
