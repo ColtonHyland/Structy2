6c0911fd922567fc2bee51557daaa85ae9730554
Colton - Work <colthyland@gmail.com>
2024-03-15 10:53:47 -0700

    soln connected components count

--- graph/connected-components-count/main.js
+++ graph/connected-components-count/main.js
@@ -4,7 +4,23 @@
 // within the graph.
 
 const connectedComponentsCount = (graph) => {
-    // todo
+    const visited = new Set();
+    let count = 0;
+    for (let node in graph) {
+        if (traverseGraph(graph, node, visited) === true) {
+            count++;
+        }
+    }
+    return count;
+};
+
+const traverseGraph = (graph, src, visited) => {
+    if (visited.has(String(src))) return false;
+    visited.add(String(src));
+    for (let neighbor of graph[src]) {
+        traverseGraph(graph, neighbor, visited);
+    }
+    return true;
 };
 
 module.exports = {
