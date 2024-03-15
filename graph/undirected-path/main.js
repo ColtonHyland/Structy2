7d7c71308cd5eb86e892af27a044f2d03d28ae75
Colton - Work <colthyland@gmail.com>
2024-03-15 10:13:32 -0700

    soln undirecrted path

--- graph/undirected-path/main.js
+++ graph/undirected-path/main.js
@@ -4,8 +4,38 @@
 // boolean indicating whether or not there exists a path between nodeA and nodeB.
 
 const undirectedPath = (edges, nodeA, nodeB) => {
-    // todo
+    const graph = fillGraph(edges);
+    return findPath(graph, nodeA, nodeB, new Set());
 };
+
+const fillGraph = (edges) => {
+    const graph = {};
+    for (let edge of edges) {
+        const [a, b] = edge;
+        if (!(a in graph)) graph[a] = [];
+        if (!(b in graph)) graph[b] = [];
+        graph[a].push(b);
+        graph[b].push(a);
+    }
+    return graph;
+};
+
+const findPath = (graph, src, dst, visited) => {
+    if (src === dst) return true;
+    if (visited.has(src)) return false;
+    visited.add(src);
+    for (let neighbor of graph[src]) {
+        if (findPath(graph, neighbor, dst, visited) === true) {
+            return true;
+        }
+    }
+    return false;
+};
+
+// n = number of nodes
+// e = number edges
+// Time: O(e)
+// Space: O(e)
 
 module.exports = {
     undirectedPath,
@@ -65,7 +95,6 @@ module.exports = {
 //   ['q', 'u'],
 //   ['q', 't'],
 // ];
-
 
 // undirectedPath(edges, 'a', 'b'); // -> true
 // const edges = [
