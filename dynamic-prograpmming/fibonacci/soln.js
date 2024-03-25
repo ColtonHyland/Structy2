b3f91edd0d9263738863fe6468d762618af9d1a4
Colton - Work <colthyland@gmail.com>
2024-03-25 10:06:25 -0700

    soln trib

--- dynamic-prograpmming/fibonacci/soln.js
+++ dynamic-prograpmming/fibonacci/soln.js
@@ -1,8 +1,7 @@
 const fib = (n, memo = {}) => {
+    if (n === 0 || n === 1) return n;
     if (n in memo) return memo[n];
-    if (n === 0) return 0;
-    if (n === 1) return 1;
-
+    
     memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
     return memo[n];
 };
@@ -10,3 +9,6 @@ const fib = (n, memo = {}) => {
 module.exports = {
     fib,
 };
+
+// Time: O(n);
+// Space: O(n);
