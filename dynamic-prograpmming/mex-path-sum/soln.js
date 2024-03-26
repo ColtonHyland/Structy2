28a978b33bd1574cdc3057ec24da645436df6be6
Colton - Work <colthyland@gmail.com>
2024-03-26 10:23:44 -0700

    init max path sum

---
+++ dynamic-prograpmming/mex-path-sum/soln.js
@@ -0,0 +1,15 @@
+const maxPathSum = (grid, r = 0, c = 0, memo = {}) => {
+    const pos = r + ',' + c;
+    if (pos in memo) return memo[pos];
+    if (r === grid.length || c === grid[0].length) return 0;
+    if (r === grid.length - 1 && c === grid[0].length - 1) return grid[r][c];
+
+    const rightSum = maxPathSum(grid, r, c + 1, memo);
+    const downSum = maxPathSum(grid, r + 1, c, memo);
+    memo[pos] = Math.max(rightSum, downSum) + grid[r][c];
+    return memo[pos];
+};
+
+module.exports = {
+    maxPathSum,
+};
