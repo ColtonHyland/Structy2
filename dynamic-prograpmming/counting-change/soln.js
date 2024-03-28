f9af2a4f0f12389b5408c009305f783b5154d3fc
Colton - Work <colthyland@gmail.com>
2024-03-28 11:05:02 -0700

    init overlap subsequence

--- dynamic-prograpmming/counting-change/soln.js
+++ dynamic-prograpmming/counting-change/soln.js
@@ -1,7 +1,8 @@
 const countingChange = (amount, coins, i = 0, memo = {}) => {
     const key = amount + ',' + i;
-    if (amount === 0) return 1;
     if (key in memo) return memo[key];
+    if (amount === 0) return 1;
+    if (i === coins.length) return 0;
 
     const coin = coins[i];
     let total = 0;
---
+++ dynamic-prograpmming/overlap-subsequence/test.js
@@ -0,0 +1,15 @@
+// overlap subsequence
+// Write a function, overlapSubsequence, that takes in two strings as arguments.
+// The function should return the length of the longest overlapping subsequence.
+
+// A subsequence of a string can be created by deleting any characters of the string,
+// while maintaining the relative order of characters.
+
+// overlapSubsequence("dogs", "daogt"); // -> 3
+// overlapSubsequence("xcyats", "criaotsi"); // -> 4
+// overlapSubsequence("xfeqortsver", "feeeuavoeqr"); // -> 5
+// overlapSubsequence("kinfolklivemustache", "bespokekinfolksnackwave"); // -> 11
+// overlapSubsequence(
+//   "mumblecorebeardleggingsauthenticunicorn",
+//   "succulentspughumblemeditationlocavore"
+// ); // -> 15
