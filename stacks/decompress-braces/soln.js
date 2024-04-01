58f4907bbe4a828140021222b61ec81a260b240f
Colton - Work <colthyland@gmail.com>
2024-04-01 09:50:54 -0700

    soln decompress braces

---
+++ stacks/decompress-braces/soln.js
@@ -0,0 +1,35 @@
+const decompressBraces = (s) => {
+    const stack = [];
+    const numberChars = '123456789';
+    for (let char of s) {
+        if (numberChars.includes(char)) {
+            stack.push(Number(char));
+        } else {
+            if (char === '}') {
+                let segment = '';
+                while (typeof stack[stack.length - 1] !== 'number') {
+                    const popped = stack.pop();
+                    segment = popped + segment;
+                }
+                const num = stack.pop();
+                stack.push(repeat(segment, num));
+            } else if (char !== '{') {
+                stack.push(char);
+            }
+        }
+    }
+
+    return stack.join('');
+};
+
+const repeat = (segment, num) => {
+    let result = '';
+    for (let i = 0; i < num; i++) {
+        result += segment;
+    }
+    return result;
+};
+
+module.exports = {
+    decompressBraces,
+};
