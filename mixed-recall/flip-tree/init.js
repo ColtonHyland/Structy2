6333d380fc02b496d926d2f0a64345370eabd32f
Colton - Work <colthyland@gmail.com>
2024-04-04 17:25:39 -0700

    init flip tree

---
+++ mixed-recall/flip-tree/init.js
@@ -0,0 +1,15 @@
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
+
+const flipTree = (root) => {
+    // todo
+};
+
+module.exports = {
+    flipTree,
+};
